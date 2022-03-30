69022f1888094bbbd584c521f30ff664590a3353
Rokas <rookas.rudzenskas@gmail.com>
2022-03-30 09:16:11 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
 import {sanityClient, urlFor} from '../sanity';
+import {Post} from "../typings";
 
 interface Props {
   posts: [Post];
 }
 
-const Home: NextPage = (props: Props) => {
+const Home = (props: Props) => {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
