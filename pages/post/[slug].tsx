8aa4921f69ed95dfa88929c1cf65bd1c4e6bdaf7
Rokas <rookas.rudzenskas@gmail.com>
2022-03-30 10:49:48 +0300

    Rokas

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {sanityClient, urlFor} from '../../sanity';
 
 const Post = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
 
 export default Post;
+
+export const getStaticPaths = async () => {
+
+}
