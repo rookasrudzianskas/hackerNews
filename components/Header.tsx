1213b8c850ab3cf453d2099aca32d0467453ba23
Rokas <rookas.rudzenskas@gmail.com>
2022-03-30 08:18:26 +0300

    Rokas

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className="">
+        <header className="flex justify-between p-5 max-w-7xl mx-auto">
             <div className="flex items-center space-x-5">
                 <Link href="/">
                     <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt=""/>
@@ -14,8 +14,9 @@ const Header = () => {
                     <h3 className="text-white bg-green-600 px-4 py-1 rounded-full cursor-pointer">Follow</h3>
                 </div>
             </div>
-            <div>
-
+            <div className="flex items-center space-x-5 text-green-600">
+                <h3>Sign In</h3>
+                <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
             </div>
         </header>
     );
