560782606b38e52c0d8e2bc1414037561f2bc24c
Rokas <rookas.rudzenskas@gmail.com>
2022-03-30 08:15:32 +0300

    Rokas

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Link from "next/link";
 
 const Header = () => {
     return (
-        <div className="">
-            I am the header
-        </div>
+        <header className="">
+            <div className="flex items-center space-x-5">
+                <Link href="/">
+                    <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt=""/>
+                </Link>
+                <div className="hidden md:inline-flex items-center space-x-5">
+                    <h3>About</h3>
+                    <h3>Contact</h3>
+                    <h3 className="text-white bg-green-600 px-4 py-1 rounded-full cursor-pointer">Follow</h3>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </header>
     );
 };
 
