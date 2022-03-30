9b4537d298dc55b76a9e8ea32f800ed3b2ab34a9
Rokas <rookas.rudzenskas@gmail.com>
2022-03-30 08:22:55 +0300

    Rokas:

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,16 @@ const Home: NextPage = () => {
 
        <Header />
 
+        <div className="">
+            <div>
+                <h1 className="text-6xl max-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span>{" "} is a place to write, read and connect</h1>
+                <h2>It is easy and free to post your thinking on any topic and connect with millions of readers</h2>
+            </div>
+            <div>
+
+            </div>
+        </div>
+
     </div>
   )
 }
