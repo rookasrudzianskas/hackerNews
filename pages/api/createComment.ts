9eb3bc561f19f81f38d74d11503dc6e19076e471
Rokas <rookas.rudzenskas@gmail.com>
2022-03-31 08:15:51 +0300

    Rokas

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,5 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {sanityClient} from "../../sanity";
 
+const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    useCdn: process.env.NODE_ENV === 'production',
+    token: process.env.SANITY_API_TOKEN,
+}
+
+const client = sanityClient(config);
 
 export default function createComment(
     req: NextApiRequest,
