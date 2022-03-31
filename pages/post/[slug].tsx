190d49a747ef2f61d4a357d944e3b8c3dc45ccac
Rokas <rookas.rudzenskas@gmail.com>
2022-03-31 08:54:28 +0300

    Rokas

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -139,13 +139,13 @@ const Post = ({post}: Props) => {
 
 
         {/*    Comments */}
-            <div className="">
-                <h3>Comments</h3>
-                <hr/>
+            <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 space-y-2 shadow">
+                <h3 className="text-4xl">Comments</h3>
+                <hr className="pb-2"/>
                 {/*@ts-ignore*/}
                 {post.comment.map((comments) => (
-                    <div className="">
-                        <p>{comments.name}:{comments.comment}</p>
+                    <div className="" key={comments._id}>
+                        <p><span className="text-yellow-500">{comments.name}:{" "}</span>{comments.comment}</p>
                     </div>
                 ))}
             </div>
